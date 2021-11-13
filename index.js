e3cb4d2a4528ae9433f5fcbc006f0c741b403460
mizan <mizanur919@gmail.com>
2021-11-13 21:22:43 +0600

    admin

--- index.js
+++ index.js
@@ -155,6 +155,17 @@ async function run() {
             const updateDoc = { $set: { role: 'admin' } };
             const result = await usersCollection.updateOne(filter, updateDoc);
             res.json(result);
+        });
+
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
         })
 
         ////////////////////////////////////////////////
